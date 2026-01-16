5ecaaa37584cc79bd4f02b33cb6c4ee0630e7f49
Nicolas Chourot <Nicolas.Chourot@clg.qc.ca>
2026-01-16 08:37:30 -0500

    Ajoutez des fichiers projet.

---
+++ Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using DAL;
+using System;
+using System.Web.Configuration;
+namespace ContactsJSON.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        const string Avatars_Folder = @"/App_Assets/Contacts/";
+        const string Default_Avatar = @"no_avatar.png";
+        [Asset(Avatars_Folder, Default_Avatar)]
+        public string Avatar { get; set; } = Avatars_Folder + Default_Avatar;
+    }
+}
